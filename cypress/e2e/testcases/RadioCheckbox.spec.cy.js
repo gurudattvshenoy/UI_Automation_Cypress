4fad27e17b4a258ae41fa7743c5f61fe1eec4102
guruvshenoy <guruvshenoy@gmail.com>
2023-03-20 20:10:43 +0530

    handling radio and checkboxes in cypress

---
+++ cypress/e2e/testcases/RadioCheckbox.spec.cy.js
@@ -0,0 +1,72 @@
+describe('Test suite for working with Radio button and Checkboxes  ',()=>{
+    it('Test case for Radio button',()=>{
+        cy.visit("https://itera-qa.azurewebsites.net/home/automation")
+        cy.get('#female').should('not.be.checked')
+        cy.get('#male').should('not.be.checked').check().should('be.checked')
+    
+    }),
+    it('Test case for checking/unchecking specific check box',()=>{
+    
+        cy.visit('https://itera-qa.azurewebsites.net/home/automation')
+       
+        //Check specific checkbox 
+        cy.get('input.form-check-input[type="checkbox"]')
+          .should('be.visible')
+          .check()
+          .should('be.checked')
+    
+          // clean up
+        cy.get('input.form-check-input[type="checkbox"]')
+          .uncheck()
+          .should('not.be.checked')
+    }),
+    it('Test case for checking and unchecking ALL check boxes',()=>{
+    
+        cy.visit('https://itera-qa.azurewebsites.net/home/automation')
+       //Check  ALL checkboxes
+        cy.get('input.form-check-input[type="checkbox"]')
+          .should('be.visible')
+          .check()
+          .should('be.checked')
+    
+          // clean up
+        cy.get('input.form-check-input[type="checkbox"]')
+          .uncheck()
+          .should('not.be.checked')
+    }),
+
+    it('Test case for checking and unchecking First Check box in the checkbox group',()=>{
+    
+      cy.visit('https://itera-qa.azurewebsites.net/home/automation')
+     //Check first checkbox
+      cy.get('input.form-check-input[type="checkbox"]')
+        .should('be.visible')
+        .first()
+        .check()
+        .should('be.checked')
+      //wait for 5 secs - Added to see in the test runner at run time
+        cy.wait(5000)
+        // clean up
+      cy.get('input.form-check-input[type="checkbox"]')
+         .first()
+        .uncheck()
+        .should('not.be.checked')
+  }),
+  it.only('Test case for checking and unchecking Last Check box in the checkbox group',()=>{
+    
+    cy.visit('https://itera-qa.azurewebsites.net/home/automation')
+   //Check last checkbox
+    cy.get('input.form-check-input[type="checkbox"]')
+      .should('be.visible')
+      .last()
+      .check()
+      .should('be.checked')
+    //wait for 5 secs - Added to see in the test runner at run time
+      cy.wait(5000)
+      // clean up
+    cy.get('input.form-check-input[type="checkbox"]')
+       .last()
+      .uncheck()
+      .should('not.be.checked')
+})
+})
